9ba2b0bfadc29e1167d64f534e842950d7b007ac
morrisonGithinji <morrison.githinji@student.moringaschool.com>
2021-10-21 20:15:44 +0300

    edit styling

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
-// import { HttpClient } from '@angular/common/http';
+ import { HttpClient } from '@angular/common/http';
 import { Users } from '../class/users';
 import { Repos } from '../class/repos';
 import { Observable, Subject } from 'rxjs';
